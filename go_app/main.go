77214e37a0974232359d0d3f2c91584bbfb08772
Jason Mcaffee <jason.mcaffee@insidesales.com>
2017-12-16 11:24:19 -0700

    simple json response implemented in go and node.

--- go_app/main.go
+++ go_app/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 	"os"
 )
 
-type(
-	Config struct{
+type (
+	Config struct {
 		Port string
+	}
+	SimpleResponse struct{
+		Hello string `json:"hello""`
 	}
 )
 
@@ -21,29 +24,22 @@ func main() {
 
 func startServer(config Config) {
 	fmt.Println("starting server with config: ", config)
-	port := ":" +config.Port
-	http.HandleFunc("/", sayhelloName)       // set router
-	err := http.ListenAndServe(port, nil) // set listen port
+	port := ":" + config.Port
+	http.HandleFunc("/simple-json-response", simpleJsonResponse) // set router
+	err := http.ListenAndServe(port, nil)                        // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func simpleJsonResponse(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("Content-Type", "application/json")
+	simpleResponse := SimpleResponse{Hello:"world"}
+	json.NewEncoder(response).Encode(simpleResponse)
 }
 
 func getConfigFromEnvVariables() Config {
 	return Config{
 		Port: os.Getenv("PORT"),
 	}
-}
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form) // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
 }
--- node-app/index.js
+++ node-app/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 
 function main(){
   console.log(`node app is running`);
@@ -7,15 +8,35 @@ function main(){
 
 function startServer({config=getConfigFromEnvVariables()}={}){
   console.log(`starting server with config: ${JSON.stringify(config)}`);
-
   const {port} = config;
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type':'text/plan'});
-    res.write('Hello Node JS Server Response');
-    res.end();
+  const server = http.createServer((request, response) => {
+    route(request, response);
   });
-
   server.listen(port);
+}
+
+function route(request, response){
+  const urlParts = url.parse(request.url);
+  const path = urlParts.pathname;
+  switch(path){
+    case '/simple-json-response':
+      simpleJsonResponse(request, response);
+      break;
+    default:
+      notFoundReponse(request, response);
+  }
+}
+
+function simpleJsonResponse(request, response){
+  const headers = {'Content-Type': 'application/json'};
+  const simpleResponse = {hello: 'world'};
+  response.writeHead(200, headers);
+  response.end(JSON.stringify(simpleResponse));
+}
+
+function notFoundReponse(request, response){
+  response.writeHead(404);
+  response.end();
 }
 
 function getConfigFromEnvVariables(){
